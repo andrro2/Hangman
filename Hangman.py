3e152b469e4e25de977f883cc20b3cfeb02c2346
András Rozner <roznernek@gmail.com>
2019-01-28 16:33:24 +0100

    v1.0.2

--- Hangman.py
+++ Hangman.py
@@ -3,5 +3,14 @@ from random import randint
 capitals = ['Sofia', 'Paris', 'Helsinki', 'Berlin', 'Athens', 'Budapest']
 
 def menu():
-    print('Welcome to Hangman game!')
+    start = 0
+    print('Welcome to Hangman game!' '\n' 'Start (1)' '\n' 'Exit(4)')
+    user_input = input()
+    if user_input == '1':
+        start = 1
+        return start
+    elif user_input == '4':
+        start = 4
+        return start
+
 
