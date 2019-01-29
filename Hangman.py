17421191b2533dc8971172c89464b64d7eae9fcd
András Rozner <roznernek@gmail.com>
2019-01-29 09:01:13 +0100

    v1.0.3

--- Hangman.py
+++ Hangman.py
@@ -5,14 +5,14 @@ capitals = ['SOFIA', 'PARIS', 'HELSINKI', 'BERLIN', 'ATHENS', 'BUDAPEST']
 
 def menu():
     start = 0
-    print('Welcome to Hangman game!' '\n' 'Start (1)' '\n' 'Exit(4)')
+    print('Welcome to Hangman game!' '\n' 'Start (1)' '\n' 'Scores(2)' '\n' 'Exit(3)')
     user_input = input()
     if user_input == '1':
         start = 1
         return start
-        
+
     elif user_input == '2':
-        start = 0
+        start = 2
         return start
 
     elif user_input == '3':
