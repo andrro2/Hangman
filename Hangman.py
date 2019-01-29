a3fcdae6fedc33813f03daef3165c62c8ae03eac
dannypepper <bors.dani@gmail.com>
2019-01-29 13:49:41 +0100

    v1.0

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+
+import time
 from random import randint
 import sys
 
 capitals = ['SOFIA', 'PARIS', 'HELSINKI', 'BERLIN', 'ATHENS', 'BUDAPEST']
+
 
 def menu():
     start = 0
@@ -14,11 +18,18 @@ def menu():
     elif user_input == '2':
         start = 2
         return start
-        pass
 
     elif user_input == '3':
         start = 3
         return start
 
+def win_lose():
 
 
+    for letter in picked():
+        print('_' * len(picked))
+    
+
+Start = time.time()
+time.process_time()
+End = time.time()
